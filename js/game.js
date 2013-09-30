92da63e7ef2e3560858456f9cf891b3e69c786ea
Jan Knigawka <jan.knigawka@gmail.com>
2013-10-01 00:49:53 +0200

    chapter 1

---
+++ js/game.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="vendor/sizzle.js" />
+/// <reference path="dom.js" />
+
+yahoo.game = (function () {
+    var dom = yahoo.dom,
+        $ = dom.$;
+
+    /* Ukrywa aktywny ekran (jeśli taki został wybrany) i wyświetla ekran 
+     * o określonym identyfikatorze. */
+    function showScreen(screenId) {
+        var activeScreen = $("#game .screen.active")[0],
+            screen = $("#" + screenId)[0];
+        if (activeScreen) {
+            dom.removeClass(activeScreen, "active");
+        }
+        dom.addClass(screen, "active");
+    }
+
+    // Odsłania metody publiczne. 
+    return {
+        showScreen: showScreen
+    };
+})();
---
+++ js/loader.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="vendor/modernizr.js" />
+/// <reference path="dom.js" />
+/// <reference path="game.js" />
+
+var yahoo = {};
+
+window.addEventListener("load", function () {
+    Modernizr.load([
+        {
+            load: [
+                "js/vendor/sizzle.js",
+                "js/dom.js",
+                "js/game.js"
+            ],
+            complete: function () {
+                yahoo.game.showScreen("spash-screen");
+            }
+        }
+    ]);
+}, false);
